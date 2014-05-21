d2f8dfa025aff9eb97e5df6d4863783266d4cb3e
João Coelho <joao@osteomics.com>
2014-05-21 21:48:12 +0100

    My version of cachematrix.R
    This is the functions I developed in order to pass the assignment in the coursera course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions that work together here.
+## The first one creates a special matrix object.
+## Secondly, it allow us to get the inverse of the matrix.
+## Saving this calculation in the cache.
+## Therefore, if a computation was already performed
+## it will not be repetead.
 
-## Write a short comment describing this function
+## It should be noted that the input matrix must be square.
+
+## makeCacheMatrix, the first function defines the actions
+## we can perform with matrixes.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- matrix(NA)
+        set <- function(m) {
+                x <<- matrix(m, nrow = sqrt(length(m)), ncol = sqrt(length(m)))
+                m <<- matrix(NA)
+        }
+        get <- function() x
+        setInv <- function(inverse) m <<- inverse
+        getInv <- function() m
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## Below is the second function, which brings us the inverse
+## of a previous calculated matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m2 <- x$getInv()
+        if(!is.na(m2[1,1])) {
+                message("getting cached data")
+                return(m2)
+        }
+        data <- x$get()
+        m2 <- solve(data, ...)
+        x$setInv(m2)
+        print(m2)
 }
+
+## For the purporse of grading and testing the functions above you can write:
+
+matrix <- makeCacheMatrix()
+## Define some numbers of your choice, as in the example below
+matrix$set(c(1,2,3,4))
+## Let's test it...
+matrix$get()
+## Ok. And now it's inverse:
+cacheSolve(matrix)
+## This last one should give you the proof that it was the inverse:
+matrix$get() %*% cacheSolve(matrix)
+
+## Thanks for your time. Hope my notes were helpful!
